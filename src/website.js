08a15bcfb85365306e1cb8bb9ae3503cd0c6d953
Lu-Smith <artistlunasmith@gmail.com>
2022-05-19 10:06:54 +0100

    Updated js for form

--- src/website.js
+++ src/website.js
@@ -1,1 +1,8 @@
-
+function confimationMessage(event) {
+  let confirmation = document.querySelector("#contact-heading");
+  confirmation.innerHTML = "Thank you. Your message has been sent 🍃";
+  let submitButton = document.querySelector("#submit");
+  submitButton.innerHTML = "Submitted";
+}
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", confimationMessage);

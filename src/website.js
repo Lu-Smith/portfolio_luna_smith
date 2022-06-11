0e194ce86c46282ae0d61a52f2eadf92e97eaaec
Lu-Smith <artistlunasmith@gmail.com>
2022-06-11 19:18:06 +0100

    Updated SEO and Illustrations page

--- src/website.js
+++ src/website.js
@@ -1,6 +1,10 @@
 //images
 
 let portfolioImage = document.querySelector("#portfolio-image");
-portfolioImage.addEventListener("click", function (portfolioImage) {
-  portfolioImage.target.classList.toggle(`portfolio-image-afterclick`);
-});
+portfolioImage.addEventListener(
+  "click",
+  function (portfolioImage) {
+    portfolioImage.target.classList.toggle(`portfolio-image-afterclick`);
+  },
+  { passive: true }
+);

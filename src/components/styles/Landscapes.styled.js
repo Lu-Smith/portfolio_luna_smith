4eb965fbed55c724ece44dad02929751a8839147
Luna Smith <artistlunasmith@gmail.com>
2022-07-08 10:53:46 +0100

    Updated Landscapes.styled for mobile devices

--- src/components/styles/Landscapes.styled.js
+++ src/components/styles/Landscapes.styled.js
@@ -21,6 +21,11 @@ export const GalleryContainer = styled.div`
     -moz-column-count: 2;
     column-count: 2;
   }
+  @media (max-width: 480px) {
+    -webkit-column-count: 1;
+    -moz-column-count: 1;
+    column-count: 1;
+  }
 `;
 
 export const BioContainer = styled.div`

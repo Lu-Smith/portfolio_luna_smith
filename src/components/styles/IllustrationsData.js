f3037de82ba8d17828a00ca9b20b7514e1f1f9a3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-06 20:08:52 +0100

    Added illustrations

--- src/components/styles/IllustrationsData.js
+++ src/components/styles/IllustrationsData.js
@@ -15,6 +15,26 @@ import Img14 from "../../images/illustrations/mermaids/KittyMermaid.jpg";
 import Img15 from "../../images/illustrations/mermaids/HedgeHogMermaid.jpg";
 import Img16 from "../../images/illustrations/mermaids/MonkeyMermaid.jpg";
 import Img17 from "../../images/illustrations/mermaids/PuppyMermaid.jpg";
+import Img18 from "../../images/illustrations/characters/Little Witch.png";
+import Img19 from "../../images/illustrations/characters/bunny.png";
+import Img20 from "../../images/illustrations/characters/bird.png";
+import Img21 from "../../images/illustrations/characters/elf2.png";
+import Img22 from "../../images/illustrations/characters/bee.png";
+import Img23 from "../../images/illustrations/characters/black cat.png";
+import Img24 from "../../images/illustrations/characters/ghosts.png";
+import Img25 from "../../images/illustrations/characters/halloween cat.png";
+import Img26 from "../../images/illustrations/characters/mole1.png";
+import Img27 from "../../images/illustrations/characters/mole2.png";
+import Img28 from "../../images/illustrations/characters/mole3.png";
+import Img29 from "../../images/illustrations/characters/mole4.png";
+import Img30 from "../../images/illustrations/items/apple.png";
+import Img31 from "../../images/illustrations/items/apple2.png";
+import Img32 from "../../images/illustrations/items/bananas.png";
+import Img33 from "../../images/illustrations/items/rock.png";
+import Img34 from "../../images/illustrations/items/halloween pumpkin.png";
+
+
+
 
 export const IllustrationsData = [
   {
@@ -84,5 +104,73 @@ export const IllustrationsData = [
   {
     image: Img17,
     alt: `Baby Puppy Mermaid- Illustration by Luna Smith`,
+  },
+  {
+    image: Img18,
+    alt: `Little Witch- Illustration by Luna Smith`,
+  },
+  {
+    image: Img19,
+    alt: `Grumpy Bunny - Illustration by Luna Smith`,
+  },
+  {
+    image: Img20,
+    alt: `Green Baby Bird- Illustration by Luna Smith`,
+  },
+  {
+    image: Img21,
+    alt: `Elf- Illustration by Luna Smith`,
+  },
+  {
+    image: Img22,
+    alt: `Bee- Illustration by Luna Smith`,
+  },
+  {
+    image: Img23,
+    alt: `Black Cat- Illustration by Luna Smith`,
+  },
+  {
+    image: Img24,
+    alt: `Ghosts- Illustration by Luna Smith`,
+  },
+  {
+    image: Img25,
+    alt: `Halloween Cat- Illustration by Luna Smith`,
+  },
+  {
+    image: Img26,
+    alt: `Mole- Illustration by Luna Smith`,
+  },
+  {
+    image: Img27,
+    alt: `Mole- Illustration by Luna Smith`,
+  },
+  {
+    image: Img28,
+    alt: `Mole- Illustration by Luna Smith`,
+  },
+  {
+    image: Img29,
+    alt: `Mole- Illustration by Luna Smith`,
+  },
+  {
+    image: Img30,
+    alt: `Red Apple- Illustration by Luna Smith`,
+  },
+  {
+    image: Img31,
+    alt: `Eaten Red Apple- Illustration by Luna Smith`,
+  },
+  {
+    image: Img32,
+    alt: `Bananas- Illustration by Luna Smith`,
+  },
+  {
+    image: Img33,
+    alt: `Rock- Illustration by Luna Smith`,
+  },
+  {
+    image: Img34,
+    alt: `Halloween Pumpkin- Illustration by Luna Smith`,
   },
 ];
